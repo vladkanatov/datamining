d37782a58a429a3527933dad6d5b4c343c1c9e8d
Vladislav <kanatiklon@gmail.com>
2024-07-06 23:30:59 +0300

    edit

--- logger.py
+++ logger.py
@@ -1,15 +1,13 @@
 from loguru import logger
-import os
-
-
-
+import sys
 def configure_logger() -> None:
-    if not os.path.exists("logs"):
-        os.makedirs("logs")
-    logger.add(
-        "logs/logs.log",
-        format='{time} - {level} - {message}',
-        rotation="10 MB",
-        retention="10 days",
-        compression="zip"
-    )
+    logger.remove()
+    logger.add(sink='logs/logs.log',
+               format="{time} {level} {message}",
+               level='INFO',
+               retention="10 days",
+               rotation="10 MB")
+    
+    logger.add(sys.stdout, format="{time} {level} {message}", level="DEBUG")
+
+    logger.info("Logger configured")
